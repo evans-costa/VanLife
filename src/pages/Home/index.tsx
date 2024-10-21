fca3ee4c6188250c2f9bbc11a42e2df399adaee8
Evandro Costa <evandro.souzac@gmail.com>
2024-10-21 16:34:18 -0300

    create `Home` page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,18 @@
+import "./index.css";
+
+export default function Home() {
+  return (
+    <div className="hero">
+      <div className="hero-content">
+        <h1 className="hero-title">
+          You got the travel plans, we got the travel vans.
+        </h1>
+        <p className="hero-description">
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
+        <button className="hero-button">Find your van</button>
+      </div>
+    </div>
+  );
+}
