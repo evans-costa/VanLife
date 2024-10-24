d1e7b838cd4f8308c1a4b8f280c476e89785bb27
Evandro Costa <evandro.souzac@gmail.com>
2024-10-24 16:03:31 -0300

    move `Layout` component to root

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,12 +1,13 @@
 import { Outlet } from "react-router-dom";
-import Header from "./Header/Header";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function Layout() {
   return (
     <>
       <Header />
       <Outlet />
-      <footer>©️ 2022 #VANLIFE</footer>
+      <Footer />
     </>
   );
 }
