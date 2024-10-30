2e964f83580de5ea8ab0130121e0d6c9a5c876d0
Evandro Costa <evandro.souzac@gmail.com>
2024-10-30 10:27:39 -0300

    use `useAuth` hook

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,26 +1,36 @@
 import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/logog.png";
 import NavBar from "../Navbar/NavBar";
-
-const navLinks = [
-  {
-    id: 1,
-    name: "Host",
-    path: "host",
-  },
-  {
-    id: 2,
-    name: "About",
-    path: "about",
-  },
-  {
-    id: 3,
-    name: "Vans",
-    path: "vans",
-  },
-];
+import { UserCircle } from "../../ui/UserCircle";
+import { useAuth } from "../../hooks/useAuth";
+import { SignIn } from "../../ui/SignIn";
 
 export default function Header() {
+  const { isAuthenticated } = useAuth();
+
+  const navLinks = [
+    {
+      id: 1,
+      name: "Host",
+      path: "host",
+    },
+    {
+      id: 2,
+      name: "About",
+      path: "about",
+    },
+    {
+      id: 3,
+      name: "Vans",
+      path: "vans",
+    },
+    {
+      id: 4,
+      name: isAuthenticated ? <UserCircle /> : <SignIn />,
+      path: isAuthenticated ? "/host" : "/login",
+    },
+  ];
+
   const navElements = navLinks.map((item) => (
     <NavLink
       key={item.id}
--- src/pages/Host/Dashboard.tsx
+++ src/pages/Host/Dashboard.tsx
@@ -1,9 +1,19 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks/useAuth";
 
 export default function Dashboard() {
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  function handleLogout() {
+    logout();
+    navigate("/");
+  }
+
   return (
     <div>
       <h1>Host Dashboard</h1>
+      <button onClick={handleLogout}>Logout</button>
       <Outlet />
     </div>
   );
